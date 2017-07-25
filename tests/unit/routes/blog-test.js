d086351c70c2d142d171b1d2efba1c072b7475b5
smoisset <smoisset@worldremit.com>
2017-07-25 21:45:26 +0100

    Update tests

--- tests/unit/routes/blog-test.js
+++ tests/unit/routes/blog-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:stories', 'Unit | Route | stories', {
+moduleFor('route:blog', 'Unit | Route | blog', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
