adda9ecfebbe9aeb39f80fad09ce508abf701ceb
smoisset <smoisset@worldremit.com>
2017-07-28 09:57:49 +0100

    Update routes for post

--- app/routes/post.js
+++ app/routes/post.js
@@ -4,4 +4,16 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('post', params.post_id);
   },
+
+  actions: {
+    updatePost(post, params) {
+      Object.keys(params).forEach(function (key) {
+        if (params[key] !== undefined) {
+          post.set(key, params[key]);
+        }
+      });
+      post.save();
+      this.transitionTo('index');
+    }
+  }
 });
