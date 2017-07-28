edc2bf412c11157a328808baf1828f9c780775b2
smoisset <smoisset@worldremit.com>
2017-07-28 15:47:12 +0100

    Add delete post feature

--- app/components/post-detail.js
+++ app/components/post-detail.js
@@ -4,6 +4,12 @@ export default Ember.Component.extend({
   actions: {
     updatePost(post, params) {
       this.sendAction('updatePost', post, params);
+    },
+
+    deletePost(post) {
+      if (confirm('Are you sure you want to delete this post?')) {
+        this.sendAction('deletePost', post);
+      }
     }
   }
 });
--- app/routes/post.js
+++ app/routes/post.js
@@ -14,6 +14,11 @@ export default Ember.Route.extend({
       });
       post.save();
       this.transitionTo('index');
+    },
+
+    deletePost(post) {
+      post.destroyRecord();
+      this.transitionTo('index');
     }
   }
 });
